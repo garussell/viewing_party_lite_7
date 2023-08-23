7c6a7183f13dc47a3c13463ff8f7faaf2403f814
Connor Richmond <richmond.connor@gmail.com>
2023-08-23 15:23:56 -0400

    Spec tests

---
+++ spec/features/discover/index_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "users discover path" do
+  before do
+    @user_1 = User.create!(name: "Ringo", email: "ringo@gmail.com")
+
+    visit user_discover_index_path(@user_1)
+  end
+
+  describe "as a user" do
+    describe "when I visit the /users/:id/discover" do
+      it "I should see a button to discover top rated movies and a text field to search" do
+        expect(page).to have_button("Find Top Rated Movies")
+        expect(page).to have_field(:title)
+        expect(page).to have_button("Find Movies")
+      end
+
+      it "they should be taken to the movies results page" do
+        VCR.use_cassette("top_rated_movies") do
+          click_on "Find Top Rated Movies"
+          expect(page).to have_current_path(user_movies_path(@user_1))
+        end
+      end
+
+      it "they should be taken to the movies results page" do
+        VCR.use_cassette("movie_search") do
+          fill_in :title, with: "Star Wars"
+          click_on "Find Movies"
+          expect(page).to have_content("Star Wars: The Clone Wars")
+          expect(page).to have_content("The Star Wars Holiday Special")
+        end
+      end
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  describe "validations" do
-
-  end
-
   describe "relationships" do
-    
+    it { should have_many(:movies) }
   end
 end
