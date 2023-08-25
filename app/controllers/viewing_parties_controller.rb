66d32fa26a8c45ff2ba262c824571bd1d2621af3
Connor Richmond <richmond.connor@gmail.com>
2023-08-25 19:57:12 -0400

    Feat: error testing

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -11,24 +11,26 @@ class ViewingPartiesController < ApplicationController
     movie_runtime = params[:runtime].to_i
     facade = MovieFacade.new(params[:id])
     @movie = facade.get_movie_by_id(params[:id]) 
-
+  
     viewing_party_params = {
-      host: @user.name,
+      host: @user,
       movie: @movie.title,
       duration: params[:viewing_party][:duration_minutes],
       viewing_time: params[:viewing_party][:start_time],
       viewing_date: params[:viewing_party][:date],
-      users: params[:viewing_party][:user_ids]
-  }
+      users: User.where(id: params[:viewing_party][:user_ids].map(&:to_i))
+    }
   
+    @viewing_party = ViewingParty.new(viewing_party_params)
+    
     if params[:viewing_party][:duration_minutes].to_i >= movie_runtime
-      @viewing_party = @user.viewing_parties.build(viewing_party_params)
-      @viewing_party.users << User.find(params[:viewing_party][:user_ids])
-
+      require 'pry'; binding.pry
       if @viewing_party.save
+        @user.viewing_parties << @viewing_party
         redirect_to user_path(@user, id: params[:id])
       else
-        flash[:error] = "Failed to create a new viewing party."
+        flash[:error] = "Failed to create a new viewing party. Errors: #{viewing_party_errors_as_string(@viewing_party)}"
+        Rails.logger.error("Viewing party save failed. Errors: #{viewing_party_errors_as_string(@viewing_party)}")
         redirect_back(fallback_location: root_path)
       end
     else
@@ -36,5 +38,12 @@ class ViewingPartiesController < ApplicationController
       redirect_back(fallback_location: root_path)
     end
   end
+
+  private
+
+  def viewing_party_errors_as_string(viewing_party)
+    viewing_party.errors.full_messages.join(", ")
+  end
+  
   
 end 
