b6dc0e601a44af56b099d7521cdebeb47d553189
Connor Richmond <richmond.connor@gmail.com>
2023-08-25 12:54:00 -0400

    merge from where issues are

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -1,5 +1,10 @@
 class DiscoverController < ApplicationController
   def index
     @user = User.find(params[:user_id])
+    if params[:search]
+      @movies = MovieFacade.search_movies(params[:search])
+    else
+      @movies = MovieFacade.top_rated_movies
+    end
   end
 end
