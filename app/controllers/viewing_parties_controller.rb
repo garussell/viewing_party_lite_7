0dfb908eb1846adb1fd5febef74b956b3b73e585
Connor Richmond <richmond.connor@gmail.com>
2023-08-25 12:53:13 -0400

    Separate data

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -1,23 +1,32 @@
 class ViewingPartiesController < ApplicationController
+
   def new
+    @viewing_party = ViewingParty.new
+    @users = User.all
     @user = User.find(params[:user_id])
-    @viewing_party = @user.viewing_parties.new
-    @movie = params[:movie_data]
+    @details = MovieDetailsFacade.movie_details(params[:movie_id])
+    @viewing_party = ViewingParty.find_by(movie_title: @details.title, host: @user.name) || ViewingParty.new
   end
 
   def create
     @user = User.find(params[:user_id])
-    @viewing_party = @user.viewing_parties.new
-    runtime = params[:runtime]
+    @details = MovieDetailsFacade.movie_details(params[:movie_id])
 
-    if params[:duration_minutes].to_i >= runtime.to_i
+    merged_params = viewing_party_params.merge(host: @user.name, movie_title: @details.title, poster_path: @details.poster_path)
 
+    @viewing_party = ViewingParty.new(merged_params)
+    if @details.runtime < @viewing_party.duration
       @viewing_party.save
-   
-      redirect_to "/users/#{@user.id}"
+      flash[:success] = "Viewing party successfully created! Now it's time to invite your friends!"
+      redirect_to "/users/#{@user.id}/movies/#{@details.id}/viewing-party/new"
     else
-      flash[:error] = "Duration cannot be less than runtime - #{runtime} minutes"
-      # render :new, locals: { movie_data: params[:movie_data] }
+      flash[:custom_message] = "Creation unsuccessful. Your viewing party can't take less time than the movie!"
+      redirect_to "/users/#{@user.id}/movies/#{@details.id}/viewing-party/new"
     end
+  end
+
+  private
+  def viewing_party_params
+    params.permit(:name, :duration, :event_date, :start_time)
   end
 end
